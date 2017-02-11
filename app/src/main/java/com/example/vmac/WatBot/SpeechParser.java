7c15ee2f56e69dd559e8082960c22702a517767a
inhibitor1217 <cytops1217@gmail.com>
2017-02-11 17:16:24 +0900

    added speechparser

--- app/src/main/java/com/example/vmac/WatBot/SpeechParser.java
+++ app/src/main/java/com/example/vmac/WatBot/SpeechParser.java
@@ -1,6 +1,11 @@
 package com.example.vmac.WatBot;
 
+import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
+import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.RecognizeCallback;
 
 /**
  * Created by Dongwook on 2017-02-11.
@@ -8,10 +13,12 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 public class SpeechParser {
 
     private SpeechToText speechService;
+    private String result;
 
     public SpeechParser() {
 
         speechService = initSpeechToTextService();
+        result = "default text";
 
     }
 
@@ -24,4 +31,45 @@ public class SpeechParser {
         return service;
     }
 
+    public String parseAudio(MicrophoneInputStream capture) {
+        speechService.recognizeUsingWebSocket(capture, getRecognizeOptions(), new MicrophoneRecognizeDelegate());
+        return result;
+    }
+
+    private RecognizeOptions getRecognizeOptions() {
+        return new RecognizeOptions.Builder()
+                .continuous(true)
+                .contentType(ContentType.OPUS.toString())
+                .model("en-US_BroadbandModel")
+                .interimResults(true)
+                .inactivityTimeout(2000)
+                .build();
+    }
+
+    private class MicrophoneRecognizeDelegate implements RecognizeCallback {
+
+        @Override
+        public void onTranscription(SpeechResults speechResults) {
+            System.out.println(speechResults);
+            result = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
+        }
+
+        @Override public void onConnected() {
+
+        }
+
+        @Override public void onError(Exception e) {
+
+        }
+
+        @Override public void onDisconnected() {
+
+        }
+
+    }
+
 }
+
+
+
+
