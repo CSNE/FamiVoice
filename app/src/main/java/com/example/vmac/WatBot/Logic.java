6cde23edf787a5ddac34162db608b6e08059441c
danny0122 <danny0122@naver.com>
2017-02-11 17:19:16 +0900

    asdf

---
+++ app/src/main/java/com/example/vmac/WatBot/Logic.java
@@ -0,0 +1,25 @@
+package com.example.vmac.WatBot;
+
+/**
+ * Created by danny on 2017-02-11.
+ */
+
+public class Logic {
+    MessageSession ms;
+
+
+    void query_start(){
+
+       ms.messageFromBot("Please tell a command");
+
+
+
+    }
+
+    void input(String message){
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/vmac/WatBot/MessageSession.java
@@ -0,0 +1,14 @@
+package com.example.vmac.WatBot;
+
+/**
+ * Created by danny on 2017-02-11.
+ */
+
+public class MessageSession {
+    public void messageFromUser(String msg){
+
+    }
+    public void messageFromBot(String msg){
+
+    }
+}
