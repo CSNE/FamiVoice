2a0bccd3038ba9c6d78e365e6396e00247c87cf1
CSNE <chansol98@gmail.com>
2017-02-11 20:44:58 +0900

    More code.

--- app/src/main/java/com/example/vmac/WatBot/MainActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 //HUNGSEO
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MessageListener{
 
 
     private RecyclerView recyclerView;
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     //StreamPlayer streamPlayer;
     //private boolean initialRequest;
     private Button recordButton;
+    MessageSession ms;
 
     private SpeechParser sp;
 
@@ -48,17 +49,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        inputMessage = (EditText) findViewById(R.id.message);
-        btnSend = (ImageButton) findViewById(R.id.btn_send);
-        String customFont = "Montserrat-Regular.ttf";
-        Typeface typeface = Typeface.createFromAsset(getAssets(), customFont);
-        inputMessage.setTypeface(typeface);
-*/
+
+        ms=new MessageSession();
+
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        messageArrayList = new ArrayList<Message>();
-        mAdapter = new ChatAdapter(messageArrayList);
+
+        mAdapter = new ChatAdapter(ms.getMessagesArrayList());
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setStackFromEnd(true);
@@ -71,137 +68,32 @@ public class MainActivity extends AppCompatActivity {
         sp=new SpeechParser();
 
 
-        //this.inputMessage.setText("");
-        //this.initialRequest = true;
-
-        //sendMessage();
-
-/*
-        //Watson Text-to-Speech Service on Bluemix
-        final  service = new TextToSpeech();
-        service.setUsernameAndPassword("Your Text-to-Speech service username", "Your Text-to-Speech password");
-*/
-
-        /*
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
-            @Override
-            public void onClick(View view, final int position) {
-                Thread thread = new Thread(new Runnable() {
-                    public void run() {
-                        Message audioMessage;
-                        try {
-
-                            audioMessage =(Message) messageArrayList.get(position);
-                            streamPlayer = new StreamPlayer();
-                            if(audioMessage != null && !audioMessage.getMessage().isEmpty())
-                                //Change the Voice format and choose from the available choices
-                                streamPlayer.playStream(service.synthesize(audioMessage.getMessage(), Voice.EN_LISA).execute());
-                            else
-                                streamPlayer.playStream(service.synthesize("No Text Specified", Voice.EN_LISA).execute());
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                thread.start();
-            }
-
-            @Override
-            public void onLongClick(View view, int position) {
-
-            }
-        }));
-        */
-
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newMessage(new Message("1","Hello!"));
-                newMessage(new Message("0","Fuck You!"));
-                if (checkInternetConnection()) {
-                    sp.parseAudio();
+
+
+
+                if (sp.isParsing()){
+                    String res=sp.stopParsing();
+                    if (res!=null) {
+                        ms.messageFromUser(res);
+                    }else{
+                        ms.messageFromUser("NULL!!!!");
+                    }
+
+                }else{
+                    if (checkInternetConnection() ) {
+                        sp.startParsing();
+                    }
 
                 }
+
+
             }
         });
     };
 
-    /*
-    // Sending a message to Watson Conversation Service
-    private void sendMessage() {
-
-        final String inputmessage = this.inputMessage.getText().toString().trim();
-        if(!this.initialRequest) {
-            Message inputMessage = new Message();
-            inputMessage.setMessage(inputmessage);
-            inputMessage.setId("1");
-            messageArrayList.add(inputMessage);
-        }
-        else
-        {
-            Message inputMessage = new Message();
-            inputMessage.setMessage(inputmessage);
-            inputMessage.setId("100");
-            this.initialRequest = false;
-            Toast.makeText(getApplicationContext(),"Tap on the message for Voice",Toast.LENGTH_LONG).show();
-
-        }
-
-        this.inputMessage.setText("");
-        mAdapter.notifyDataSetChanged();
-
-        Thread thread = new Thread(new Runnable(){
-            public void run() {
-                try {
-
-        ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_09_20);
-        service.setUsernameAndPassword("Your Watson service UserName", "Your watson service PassWord");
-        MessageRequest newMessage = new MessageRequest.Builder().inputText(inputmessage).context(context).build();
-        MessageResponse response = service.message("Your Workspace Id", newMessage).execute();
-
-                    //Passing Context of last conversation
-                if(response.getContext() !=null)
-                    {
-                        context.clear();
-                        context = response.getContext();
-
-                    }
-        Message outMessage=new Message();
-          if(response!=null)
-          {
-              if(response.getOutput()!=null && response.getOutput().containsKey("text"))
-              {
-
-                  final String outputmessage = response.getOutput().get("text").toString().replace("[","").replace("]","");
-                  outMessage.setMessage(outputmessage);
-                  outMessage.setId("2");
-                  messageArrayList.add(outMessage);
-              }
-
-              runOnUiThread(new Runnable() {
-                  public void run() {
-                      mAdapter.notifyDataSetChanged();
-                     if (mAdapter.getItemCount() > 1) {
-                          recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, mAdapter.getItemCount()-1);
-
-                      }
-
-                  }
-              });
-
-
-          }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
-
-    }
-*/
 
     /**
      * Check Internet Connection
@@ -228,13 +120,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void newMessage(Message newMessage){
-        messageArrayList.add(newMessage);
+    @Override
+    public void newMessageFromUser(String msg) {
         mAdapter.notifyDataSetChanged();
     }
 
-
-
-
+    @Override
+    public void newMessageFromBot(String msg) {
+        mAdapter.notifyDataSetChanged();
+    }
 }
 
--- app/src/main/java/com/example/vmac/WatBot/MessageSession.java
+++ app/src/main/java/com/example/vmac/WatBot/MessageSession.java
@@ -1,17 +1,30 @@
 package com.example.vmac.WatBot;
 
+import java.util.ArrayList;
+
 /**
  * Created by danny on 2017-02-11.
  */
 
 public class MessageSession {
-    public void messageFromUser(String msg){
 
+
+    ArrayList<MessageListener> mls=new ArrayList<>();
+
+    ArrayList<Message> messages=new ArrayList<>();
+
+    public ArrayList<Message> getMessagesArrayList(){
+        return messages;
     }
+
+    public void messageFromUser(String msg){
+        messages.add(new Message());
+    }
+
     public void messageFromBot(String msg){
 
     }
     public void addMessageListener(MessageListener ml){
-
+        mls.add(ml);
     }
 }
