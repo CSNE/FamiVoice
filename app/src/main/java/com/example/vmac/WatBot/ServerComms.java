5abb2cecc68202584e9b94003a5a8f3f2e98381c
danny0122 <danny0122@naver.com>
2017-02-11 21:01:45 +0900

    asdf

---
+++ app/src/main/java/com/example/vmac/WatBot/ServerComms.java
@@ -0,0 +1,41 @@
+package com.example.vmac.WatBot;
+
+/**
+ * Created by danny on 2017-02-11.
+ */
+
+public class ServerComms {
+    private ServerComms(){
+
+    }
+
+    private static ServerComms inst=new ServerComms();
+
+    public ServerComms getInstance(){
+        return inst;
+    }
+
+    public void requestAllLocations(){
+
+    }
+
+    public void requestSomeoneLocations(){
+
+    }
+
+    public void requestAllTasks(){
+
+    }
+
+    public void requestMyTasks(){
+
+    }
+
+    public void deleteTask(int n){
+
+    }
+
+    public void addTask(String task){
+
+    }
+}
