35e0b9c310080458af65525be1d44ca72d214bbe
inhibitor1217 <cytops1217@gmail.com>
2017-02-12 16:01:57 +0900

    FamiVoice Finally Works!!!

--- app/src/main/java/com/example/vmac/WatBot/Logic.java
+++ app/src/main/java/com/example/vmac/WatBot/Logic.java
@@ -78,7 +78,7 @@ public class Logic implements MessageListener{
             String[] command=newmsg.split(" ");
             int len=command.length;
 
-            if(len<3){   ms.messageFromBot("The command is not available. Please try again"); return;}
+            if(len<3){   ms.messageFromBot("This command is not available. Please try again"); return;}
 
             if(command[0].equals("ask")&&command[1].equals("location")){
 
@@ -92,18 +92,18 @@ public class Logic implements MessageListener{
 
                 if(command[2].equals("on")){}
                 else if(command[2].equals("of")){}  // todo 문자 정보기능 껴고 키기
-                else {   ms.messageFromBot("The command is not available. Please try again"); return;}
+                else {   ms.messageFromBot("This command is not available. Please try again"); return;}
 
             }// update location on or off
             else if(command[0].equals("ask")&&command[1].equals("task")){
 
                 if(command[2].equals("list")){ sc.requestAllTasks(); }  // 전부
                 else if(command[2].equals("mine")){ sc.requestMyTasks(); }  // 내꺼만
-                else {   ms.messageFromBot("The command is not available. Please try again"); return;}
+                else {   ms.messageFromBot("This command is not available. Please try again"); return;}
 
             } // ask task list of mine
 
-            if(len<4){   ms.messageFromBot("The command is not available. Please try again"); return;}
+            if(len<4){   ms.messageFromBot("This command is not available. Please try again"); return;}
 
             if(command[0].equals("update")&&command[1].equals("task")){
 
@@ -119,12 +119,12 @@ public class Logic implements MessageListener{
                     StringBuffer num = new StringBuffer(command[3]);
                     for(int i=4;i<len;i++) num.append(command[i]); // 그 뒤로 이어진 단어들을 연결
                     n=wn.word_to_number(num.toString());
-                    if(n<=1){   ms.messageFromBot("The number is not available. Please try again"); return;}
+                    if(n<=1){   ms.messageFromBot("This index is not available. Please try again"); return;}
                     sc.deleteTask(n); // 말한 숫자 번째의 할 일 지우기
                     // todo 총 할일 수보다 큰 숫자인지 확인
 
                 }
-                else {   ms.messageFromBot("The command is not available. Please try again"); return;}
+                else {   ms.messageFromBot("This command is not available. Please try again"); return;}
             }
 
             // todo 서버에서 받은 정보 출력하기
